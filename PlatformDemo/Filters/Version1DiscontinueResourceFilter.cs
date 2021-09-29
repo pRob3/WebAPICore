297f7fe616ce292a76457f6ec24b6081fb4e8e8c
pRob3 <bubblan@slaf.se>
2021-09-29 15:42:18 +0200

    ResourceFilter

---
+++ PlatformDemo/Filters/Version1DiscontinueResourceFilter.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+
+namespace PlatformDemo.Filters
+{
+    public class Version1DiscontinueResourceFilter : Attribute, IResourceFilter
+    {
+        public void OnResourceExecuted(ResourceExecutedContext context)
+        {
+        }
+
+        public void OnResourceExecuting(ResourceExecutingContext context)
+        {
+            if (!context.HttpContext.Request.Path.Value.ToLower().Contains("v2"))
+            {
+                // Short circut
+                context.Result = new BadRequestObjectResult(
+                    new {
+                        Versioning = new[]{"This version of the API has expired, please use the latest version."}
+                    }
+                );
+            }
+        }
+    }
+}
