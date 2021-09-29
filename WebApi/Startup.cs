0eaa86b7393e26b1d57a8c21d6e19f64191aba04
pRob3 <bubblan@slaf.se>
2021-09-29 17:10:44 +0200

    Added EF

--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -1,6 +1,8 @@
+using DataStore.EF;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -12,19 +14,39 @@ namespace WebApi
 {
     public class Startup
     {
+        private readonly IWebHostEnvironment _env;
+
+        public Startup(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            if (_env.IsDevelopment())
+            {
+                services.AddDbContext<BugsContext>(options =>
+                {
+                    options.UseInMemoryDatabase("Bugs");
+                });
+            }
+
             services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, BugsContext context)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+
+                // Create the in-memory database for dev enviroment
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+
             }
 
             app.UseRouting();
