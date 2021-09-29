4fd1005d758b5469579f8a99882e1d54ca7f54e3
pRob3 <bubblan@slaf.se>
2021-09-29 11:22:49 +0200

    Complex type Model binding from route and query

--- PlatformDemo/Controllers/ProjectsController.cs
+++ PlatformDemo/Controllers/ProjectsController.cs
@@ -1,9 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using PlatformDemo.Models;
 
 namespace PlatformDemo.Controllers
 {
@@ -22,6 +18,30 @@ namespace PlatformDemo.Controllers
         {
             return Ok($"Reading project #{id}.");
         }
+
+        //[HttpGet]
+        //[Route("/api/projects/{pid}/tickets")]
+        //public IActionResult GetProjectTicket(int pId, [FromQuery] int tId)
+        //{
+        //    if (tId == 0)
+        //        return Ok($"Reading all the tickets belong to project #{pId}.");
+
+        //    return Ok($"Reading project #{pId}, ticket #{tId}.");
+        //}
+
+        [HttpGet]
+        [Route("/api/projects/{pid}/tickets")]
+        public IActionResult GetProjectTicket([FromQuery]Ticket ticket)
+        {
+            if (ticket == null)
+                return BadRequest("Parameters are not provided properly!");
+
+            if (ticket.TicketId == 0)
+                return Ok($"Reading all the tickets belong to project #{ticket.ProjectId}.");
+
+            return Ok($"Reading project #{ticket.ProjectId}, ticket #{ticket.TicketId}, title: {ticket.Title}, description: {ticket.Description}.");
+        }
+
 
         [HttpPost]
         public IActionResult Post()
---
+++ PlatformDemo/Models/Ticket.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlatformDemo.Models
+{
+    public class Ticket
+    {
+        [FromQuery(Name = "tId")]
+        public int TicketId { get; set; }
+        [FromRoute(Name = "pId")]
+        public int ProjectId { get; set; }
+
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
